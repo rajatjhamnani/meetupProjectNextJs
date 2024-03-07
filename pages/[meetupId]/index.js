2074c18a56112ddd590180276a2ae3a842dddf0f
rajatjhamnani <rajatjhamnani@gmail.com>
2024-03-08 02:34:12 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,34 +1,34 @@
 import { useRouter } from "next/router";
 
-const DetailPage = () => {
-  const Dummy_Meetups = [
-    {
-      id: "m1",
-      title: "A first Meetup ",
-      image:
-        "https://en.wikipedia.org/wiki/Katni#/media/File:Kataighat-katni.jpg",
-      address: "nai basti katni",
-      description: "this is my first meet up",
-    },
-    {
-      id: "m2",
-      title: "A second Meetup ",
-      image:
-        "https://en.wikipedia.org/wiki/Jabalpur#/media/File:Marble_Rocks_-_Jabalpur.jpg",
-      address: "lal mati jabalpur",
-      description: "this is my second  meet up",
-    },
-  ];
+const DetailPage = ({ meetupData }) => {
+  // const Dummy_Meetups = [
+  //   {
+  //     id: "m1",
+  //     title: "A first Meetup ",
+  //     image:
+  //       "https://en.wikipedia.org/wiki/Katni#/media/File:Kataighat-katni.jpg",
+  //     address: "nai basti katni",
+  //     description: "this is my first meet up",
+  //   },
+  //   {
+  //     id: "m2",
+  //     title: "A second Meetup ",
+  //     image:
+  //       "https://en.wikipedia.org/wiki/Jabalpur#/media/File:Marble_Rocks_-_Jabalpur.jpg",
+  //     address: "lal mati jabalpur",
+  //     description: "this is my second  meet up",
+  //   },
+  // ];
 
   const router = useRouter();
   const meetupId = router.query.meetupId;
   console.log(meetupId);
 
-  const selectedMeetup = Dummy_Meetups.find((meetup) => meetup.id === meetupId);
+  const selectedMeetup = meetupData.find((meetup) => meetup.id === meetupId);
 
-  if (!selectedMeetup) {
-    return <h1>Meetup not found</h1>;
-  }
+  // if (!selectedMeetup) {
+  //   return <h1>Meetup not found</h1>;
+  // }
 
   return (
     <>
@@ -39,5 +39,63 @@ const DetailPage = () => {
     </>
   );
 };
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+export async function getStaticProps(context) {
+  // can fetch data
+  const Dummy_Meetups = [
+    {
+      id: "m1",
+      title: "A first Meetup ",
+      image:
+        "https://th.bing.com/th/id/OIP.VJAS1OxOIcc_3AFK_mLdvQHaEb?rs=1&pid=ImgDetMain",
+      address: "nai basti katni",
+      description: "this is my first meet up",
+    },
+    {
+      id: "m2",
+      title: "A second Meetup ",
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/b4/bd/56/view-of-the-waterfall.jpg?w=1200&h=1200&s=1",
+      address: "lal mati jabalpur",
+      description: "this is my second  meet up",
+    },
+    {
+      id: "m3",
+      title: "A third Meetup ",
+      image:
+        "https://th.bing.com/th/id/R.0e6b2d34a1e66cfe4bc5487d1c31e047?rik=lftswG2fcjeILQ&riu=http%3a%2f%2furbanupdate.in%2fwp-content%2fuploads%2f2019%2f03%2fbhopal-lake.jpg&ehk=V14TnsdKfqtGXVuZtyqBdHJmvY11q2Ejl9VMzkWGRV0%3d&risl=&pid=ImgRaw&r=0",
+      address: " city of LakesBhopal",
+      description: "this is my third  meet up",
+    },
+  ];
+
+  //const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: Dummy_Meetups,
+    },
+  };
+}
 
 export default DetailPage;
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const HomePage = () => {
       id: "m1",
       title: "A first Meetup ",
       image:
-        "https://en.wikipedia.org/wiki/Katni#/media/File:Kataighat-katni.jpg",
+        "https://th.bing.com/th/id/OIP.VJAS1OxOIcc_3AFK_mLdvQHaEb?rs=1&pid=ImgDetMain",
       address: "nai basti katni",
       description: "this is my first meet up",
     },
@@ -14,9 +14,17 @@ const HomePage = () => {
       id: "m2",
       title: "A second Meetup ",
       image:
-        "https://en.wikipedia.org/wiki/Jabalpur#/media/File:Marble_Rocks_-_Jabalpur.jpg",
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/b4/bd/56/view-of-the-waterfall.jpg?w=1200&h=1200&s=1",
       address: "lal mati jabalpur",
       description: "this is my second  meet up",
+    },
+    {
+      id: "m3",
+      title: "A third Meetup ",
+      image:
+        "https://th.bing.com/th/id/R.0e6b2d34a1e66cfe4bc5487d1c31e047?rik=lftswG2fcjeILQ&riu=http%3a%2f%2furbanupdate.in%2fwp-content%2fuploads%2f2019%2f03%2fbhopal-lake.jpg&ehk=V14TnsdKfqtGXVuZtyqBdHJmvY11q2Ejl9VMzkWGRV0%3d&risl=&pid=ImgRaw&r=0",
+      address: " city of LakesBhopal",
+      description: "this is my third  meet up",
     },
   ];
 
